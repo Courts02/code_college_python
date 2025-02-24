2e975c17df76b4cb04f22621c2fa04c67e87d26c
Courts02 <courtneycook2006@gmail.com>
2025-02-24 15:23:40 +0200

    Chapters 1 and 2 Python

---
+++ Chapter_2/my_variables.py
@@ -0,0 +1,46 @@
+# name = "Courtney"
+
+# surname = "Dewing"
+
+# print(name + " " + surname)
+
+# my_sentence = 'courts said the following: "Hello"'
+
+# print(my_sentence.title())
+
+# my_initials = f"{name} {surname}"
+
+# print(f"\t{my_initials}")
+
+# print("         Courts".lstrio())
+
+# my_link = "https://codecollege.co.za"
+# print(my_link.removeprefix("httops//"))
+
+# f string w/ 3 var 
+
+# name = "Courtney"
+
+# age = 18
+
+# city = "Cape Town"
+
+# message = f"Hello, my name is {name}, I am {age} years old, and I live in {city}."
+
+# print(message)
+
+# print("\tI dont know what to put here.\n so suffer with me[=\n Still dont know~")
+
+# formula = 3*7
+
+# print(formula)
+
+# 4 var in same line assign value and print
+
+a, b, c, d = 10, 20, 30, 40
+
+print(b)
+
+import this 
+#  The Zen of Python, by Tim Peters
+#  Beautiful is better than ugly.
